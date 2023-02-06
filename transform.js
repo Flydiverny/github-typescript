3206724498669e612bdfb47d2269d2c833f68eb3
Markus Maga <markus@maga.se>
2023-02-06 22:12:19 +0100

    test
    Signed-off-by: Markus Maga <markus@maga.se>

--- transform.js
+++ transform.js
@@ -4,6 +4,6 @@ require("esbuild").buildSync({
   bundle: true,
   platform: "node",
   packages: "external",
-  absWorkingDir: path.dirname(process.env.SCRIPT_FILE),
+  // absWorkingDir: path.resolve(path.dirname(process.env.SCRIPT_FILE)),
   outdir: path.dirname(process.env.SCRIPT_FILE),
 });
